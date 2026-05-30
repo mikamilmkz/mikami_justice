848dcec2e0dfd475915e1db060dd2cec1cfa9c05
mikamilmkz <mikami41kl@outlook.fr>
2026-05-31 01:24:12 +0200

    fix railway express fallback route

--- black-box-site/server.js
+++ black-box-site/server.js
@@ -246,7 +246,7 @@ app.post("/api/search", async (req, res) => {
 
 app.use(express.static(path.join(__dirname, "dist")));
 
-app.get("*", (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
